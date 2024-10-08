6f7cc8e1076d394ea14c4b6f67e647d3eda704fd
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-08 15:57:52 -0400

    Adding 6 vehicle into the inventory

--- VehicleInventory/src/main/java/com/pluralsight/Main.java
+++ VehicleInventory/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,23 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Vehicle[] inventory;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public static Vehicle[] GetInitialInventory(){
+        Vehicle[] inventory = new Vehicle[20];
+        inventory[0] = new Vehicle(101121,"Ford Explorer","Red",45000,13500);
+        inventory[0] = new Vehicle(101122,"Toyota Camry","Blue",60000,11000);
+        inventory[0] = new Vehicle(101123,"Chevrolet Malibu","Black",50000,9700);
+        inventory[0] = new Vehicle(101124,"Honda Civic","White",70000,7500);
+        inventory[0] = new Vehicle(101125,"Subaru Outback","Green",55000,14500);
+        inventory[0] = new Vehicle(101126,"Jeep Wrangler","Yellow",30000,16000);
+        return inventory;
     }
 }
---
+++ VehicleInventory/src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private long vehicleId;
+    private String makeModel;
+    private String color;
+    private int odometerReading;
+    private float price;
+
+
+    public Vehicle(long vehicleId, String makeModel, String color, int odometerReading, float price) {
+        this.vehicleId = vehicleId;
+        this.makeModel = makeModel;
+        this.color = color;
+        this.odometerReading = odometerReading;
+        this.price = price;
+    }
+
+    public long getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+}
